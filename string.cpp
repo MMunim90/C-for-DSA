10bb1a38718eb01983d05f2a37c00dbfa7ec6c3c
MMunim <mmunim9.9.01@gmail.com>
2025-11-16 19:36:20 +0600

    string on c++

---
+++ string.cpp
@@ -0,0 +1,38 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char s[100];
+//     // cin >> s;
+//     // in c we can take input in string with space by fgets(s,100,stdin); these work also in c++,
+
+//     cin.getline(s, 100);
+//     cout << s << endl;
+//     return 0; 
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int x;
+//     cin >> x;
+//     cin.ignore();
+//     char s[100];
+//     // cin >> s;
+//     cin.getline(s, 100);
+//     cout << x << endl << s << endl;
+//     return 0; 
+// }
+
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s;
+    return 0; 
+}
