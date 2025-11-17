b16d5009efff94712f856736eb2dc4883d401b3d
MMunim <mmunim9.9.01@gmail.com>
2025-11-17 19:49:22 +0600

    explore dynamic variable and array concept

---
+++ Module 2/dynamic_array.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // static array
+    // int a[5];
+    // for(int i=0; i<5; i++)
+    // {
+    //     cin >> a[i];
+    // }
+    // for(int i=0; i<5; i++)
+    // {
+    //     cout << a[i] << " ";
+    // }
+
+    // dynamic array
+    int *a = new int[5];
+    for(int i=0; i<5; i++)
+    {
+        cin >> a[i];
+    }
+    for(int i=0; i<5; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
