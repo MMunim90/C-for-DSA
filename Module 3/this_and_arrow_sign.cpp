c16c2ab7d16e39d6c80c6b2c010f6dbf7aa80c98
MMunim <mmunim9.9.01@gmail.com>
2025-11-20 17:33:41 +0600

    explore this keyword and arrow function

---
+++ Module 3/this_and_arrow_sign.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    int roll;
+    int cls;
+    double gpa;
+
+    Student(int roll, int cls, double gpa)
+    {
+        // this->roll = roll;
+        // this->cls = cls;
+        // this->gpa = gpa;
+        
+        // "this" is a pointer
+        (*this).roll = roll;
+        (*this).cls = cls;
+        (*this).gpa = gpa;
+    }
+};
+
+int main()
+{
+    Student munim(17, 10, 5.00);
+    Student karim(27, 10, 4.72);
+
+
+    cout << munim.roll << " " << munim.cls << " " << munim.gpa << endl;
+    cout << karim.roll << " " << karim.cls << " " << karim.gpa << endl;
+    return 0;
+}
