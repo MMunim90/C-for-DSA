81fc2c4cff496e05aecfc809095e7b96d485ccda
MMunim <mmunim9.9.01@gmail.com>
2025-11-22 09:37:06 +0600

    practice problem 1 done

--- Practice day 2/practice_1.cpp
+++ Practice day 2/practice_1.cpp
@@ -10,9 +10,8 @@ public:
     int math_marks;
     int cls;
 
-    Student( char name[20], int roll, char section, int math_marks, int cls)
+    Student( int roll, char section, int math_marks, int cls)
     {
-        this->name[20] = name[20];
         this->roll = roll;
         this->section = section;
         this->math_marks = math_marks;
@@ -22,9 +21,17 @@ public:
 
 int main()
 {
-    Student student_1( "munim" ,17, 'A', 72, 10);
-    Student student_2( "amin" ,27, 'A', 68, 10);
-    Student student_3( "roni" ,35, 'A', 62, 10);
+    Student student_1( 17, 'A', 72, 10);
+    char temp_1[20] = "munim";
+    strcpy(student_1.name, temp_1);
+
+    Student student_2( 27, 'A', 68, 10);
+    char temp_2[20] = "amin";
+    strcpy(student_2.name, temp_2);
+
+    Student student_3( 35, 'A', 62, 10);
+    char temp_3[20] = "roni";
+    strcpy(student_3.name, temp_3);
 
     if(student_1.math_marks > student_2.math_marks && student_1.math_marks > student_3.math_marks)
     {
@@ -32,14 +39,14 @@ int main()
     }
     else if(student_2.math_marks > student_1.math_marks && student_2.math_marks > student_3.math_marks)
     {
-        cout << student_2.name[20] << endl;
+        cout << student_2.name << endl;
     }
     else if(student_3.math_marks > student_2.math_marks && student_3.math_marks > student_1.math_marks)
     {
-        cout << student_3.name[20] << endl;
+        cout << student_3.name << endl;
     }
 
-    // cout << student_1.name[20] << " " << student_1.roll << " " << student_1.section << " " << student_1.math_marks << " " << student_1.cls << endl;
+    // cout << student_1.name << " " << student_1.roll << " " << student_1.section << " " << student_1.math_marks << " " << student_1.cls << endl;
 
 
     return 0;
