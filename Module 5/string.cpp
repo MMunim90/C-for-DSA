da3210827b29b0564cc9e21c755a12de7564621b
MMunim <mmunim9.9.01@gmail.com>
2025-11-26 21:58:10 +0600

    explore string in c++

---
+++ Module 5/string.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // using charecter datatype
+    // char s[10] = "hello";
+    // // s = "gello"; // not possible
+    // strcpy(s, "gello");
+    // cout << s << endl;
+
+
+    // using string datatype
+    // string s = "hello";
+    // s = "gello";
+    // cout << s << endl;
+
+
+    // char s[10] = "hefllo";
+    // char s2[10] = "hello";
+
+    // if(strcmp(s, s2) == 0)
+    // {
+    //     cout << "same" << endl;
+    // }
+    // else
+    // {
+    //     cout << "not same" << endl;
+    // }
+
+
+    // string s = "hello";
+    // string s2 = "hello";
+
+    // if(s == s2)
+    // {
+    //     cout << "same" << endl;
+    // }
+    // else
+    // {
+    //     cout << "not same" << endl;
+    // }
+
+
+    string s = "munim";
+    s = "munim want to be a good problem solver and programmer";
+
+    cout << s << endl;
+    return 0;
+}
