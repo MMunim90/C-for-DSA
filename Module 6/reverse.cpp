dfa03019bc3f330d77cc35cb72ba887831e3fed7
MMunim <mmunim9.9.01@gmail.com>
2025-11-28 15:14:14 +0600

    explore reverse function

---
+++ Module 6/reverse.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    // integer array reverse
+    // int n;
+    // cin >> n;
+    // int arr[n];
+    // for(int i=0; i<n; i++)
+    // {
+    //     cin >> arr[i];
+    // }
+
+    // reverse(arr, arr+n);
+
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout << arr[i] << " ";
+    // }
+
+
+    // string reverse
+
+    string s;
+    cin >> s;
+    reverse(s.begin(), s.end());
+
+    cout << s << endl;
+    
+    return 0;
+}
