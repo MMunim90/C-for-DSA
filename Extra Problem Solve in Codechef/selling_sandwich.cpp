dbb9f7f7d7b0824d28dff563f2e1735e32d625bf
MMunim <mmunim9.9.01@gmail.com>
2025-12-07 21:52:13 +0600

    selling sandwitch problem solved

---
+++ Extra Problem Solve in Codechef/selling_sandwich.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    int amount = a - (b + c);
+    
+    cout << amount << endl;
+    return 0;
+}
