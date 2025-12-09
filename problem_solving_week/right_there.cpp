9d5d1394a48be6570d92d40c80ed6eb4f45c53d5
MMunim <mmunim9.9.01@gmail.com>
2025-12-09 18:35:15 +0600

    right there problem solved

---
+++ problem_solving_week/right_there.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int x, y; cin >> x >> y;
+        (x > y) ? cout << "NO" << endl : cout << "YES" << endl; 
+    }
+    
+    return 0;
+}
