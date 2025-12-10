eddca48a7ff319ec43ef6e5348fd8a07ccd17de6
MMunim <mmunim9.9.01@gmail.com>
2025-12-10 18:52:26 +0600

    cloud watching problem solved

---
+++ Extra Problem Solve in Codechef/cloud_watching.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b; cin >> a >> b;
+    if(a*3 <= b)
+    {
+        cout << "Rain" << endl;
+    }
+    else 
+    {
+        cout << "Dry" << endl;
+    }
+    return 0;
+}
