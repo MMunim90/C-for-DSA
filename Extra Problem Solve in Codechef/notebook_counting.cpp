2011dee60fc07a485c7ad241b9ee183f1c40be9a
MMunim <mmunim9.9.01@gmail.com>
2025-12-10 22:56:10 +0600

    notebook counting problem solved

---
+++ Extra Problem Solve in Codechef/notebook_counting.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b, total_lines; cin >> a >> b;
+    total_lines = (b*50)*2*a;
+    cout << total_lines << endl;
+    return 0;
+}
