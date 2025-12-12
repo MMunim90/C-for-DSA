c30462a411f16d5b05df863cb611d7b1e1176b68
MMunim <mmunim9.9.01@gmail.com>
2025-12-12 10:22:35 +0600

    chocolate chocolate problem solve without recursion

--- codefest/chocolate_chocolate.cpp
+++ codefest/chocolate_chocolate.cpp
@@ -1,20 +1,21 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-void chocolate(int n)
-{
-    
-}
 
 int main()
 {
-    int t; cin >> t;
+    int t;
+    cin >> t;
     while (t--)
     {
-        int n; cin >> n;
+        int tk, choco;
+        cin >> tk;
 
-        chocolate(n);
+        choco = tk / 5;
+       
+        choco += choco/2;
+
+        cout << choco << endl;
     }
-    
+
     return 0;
 }
